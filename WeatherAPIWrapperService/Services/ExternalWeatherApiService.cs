24688cea603b0762ae392c9848cee345aebdc453
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-21 02:10:48 +0400

    feat: add ExternalWeatherApiService

---
+++ WeatherAPIWrapperService/Services/ExternalWeatherApiService.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using WeatherAPIWrapperService.Models;
+
+namespace WeatherAPIWrapperService.Services
+{
+    public class ExternalWeatherApiService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public ExternalWeatherApiService(IHttpClientFactory httpClientFactory, IConfiguration configuration) 
+        { 
+            this._httpClientFactory = httpClientFactory;
+            this._configuration = configuration;
+        }
+
+        public async Task<WeatherResponse> GetWeatherData(string city)
+        {
+            HttpClient client = _httpClientFactory.CreateClient();
+            
+            var response = await client.GetAsync($"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key={_configuration["WeatherAPI:Key"]}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Error");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var apiWeatherResponse = JsonSerializer.Deserialize<ApiWeatherResponse>(json, options);
+
+            var weahterResponse = new WeatherResponse()
+            {
+                ResolvedAddress = apiWeatherResponse.ResolvedAddress,
+                Day = apiWeatherResponse.Days[0]
+            };
+
+            return weahterResponse;
+        }
+    }
+}
