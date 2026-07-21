720f27ec2a9deebb749e26451c98d35091d8c61d
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-21 08:56:45 +0400

    feat: add WeatherService integrating Redis cache and external API

---
+++ WeatherAPIWrapperService/Services/WeatherService.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using WeatherAPIWrapperService.Models;
+
+namespace WeatherAPIWrapperService.Services
+{
+    public class WeatherService
+    {
+        private readonly RedisService _redis;
+        private readonly ExternalWeatherApiService _externalWeatherApiService;
+
+        public WeatherService(RedisService redis, ExternalWeatherApiService externalWeatherApiService) 
+        {
+            this._redis = redis;
+            this._externalWeatherApiService = externalWeatherApiService;
+        }
+
+        public async Task<WeatherResponse?> GetWeatherResponseAsync(string city)
+        {
+            var weatherResponse = await _redis.GetAsync(city);
+            if (weatherResponse == null)
+            {
+                weatherResponse = await _externalWeatherApiService.GetWeatherData(city);
+                if (weatherResponse != null)
+                {
+                    await _redis.SetAsync(weatherResponse, city, TimeSpan.FromHours(1));
+                }
+            }
+
+            return weatherResponse;
+        }
+    }
+}
