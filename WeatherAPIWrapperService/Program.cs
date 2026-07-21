8b0239606d05169617d25592b5bc8003c20394b1
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-21 08:57:52 +0400

    feat: register services and HttpClient in dependency injection

--- WeatherAPIWrapperService/Program.cs
+++ WeatherAPIWrapperService/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using StackExchange.Redis;
+using WeatherAPIWrapperService.Models;
+using WeatherAPIWrapperService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost:6379"));
+builder.Services.AddScoped<RedisService>();
+builder.Services.AddScoped<ExternalWeatherApiService>();
+builder.Services.AddScoped<WeatherService>();
+builder.Services.AddHttpClient();
+
 
 var app = builder.Build();
 
